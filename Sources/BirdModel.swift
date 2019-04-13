6c2524b89afb5a6f1d5202e459609acc7e91002e
Tom Sullivan <tom@msbit.com.au>
2019-04-13 11:21:11 +1000

    Initial

---
+++ Sources/BirdModel.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+public class BirdModel {
+    public enum Rarity {
+        case common
+        case uncommon
+        case rare
+        case veryRare
+    }
+    
+    let name: String
+    let rarity: Rarity
+    let image: UIImage
+    
+    public init(name: String, rarity: Rarity, image: UIImage) {
+        self.name = name
+        self.rarity = rarity
+        self.image = image
+    }
+}
---
+++ Sources/BirdViewModel.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+public class BirdViewModel {
+    let model: BirdModel
+    
+    public init(model: BirdModel) {
+        self.model = model
+    }
+    
+    public var name: String {
+        return model.name
+    }
+    
+    public var image: UIImage {
+        return model.image
+    }
+    
+    public var purchaseFeeText: String {
+        switch model.rarity {
+        case .common:
+            return "$40"
+        case .uncommon:
+            return "$65"
+        case .rare:
+            return "$100"
+        case .veryRare:
+            return "$300"
+        }
+    }
+}
